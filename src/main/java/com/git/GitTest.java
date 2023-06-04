cdcf4a7c0bdbc7c4b072c6a0f5d94de7e5c2778b
liudan <2518572131@qq.com>
2023-06-04 17:28:27 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.git;
+
+import jdk.nashorn.internal.parser.JSONParser;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
